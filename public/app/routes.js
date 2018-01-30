f7359196a6e3b7266b49bcf88e430fc59a811f51
Maxim <maximgen666@gmail.com>
2018-01-30 22:05:01 +0300

    route permission

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,4 +1,4 @@
-angular.module('appRoutes', ['ngRoute'])
+var app = angular.module('appRoutes', ['ngRoute'])
 
     .config(function ($routeProvider, $locationProvider) {
 
@@ -35,19 +35,23 @@ angular.module('appRoutes', ['ngRoute'])
 
             .when('/notes', {
                 templateUrl:'app/views/pages/template/list.html',
-                controller:'boardCtrl'
+                controller:'boardCtrl',
+                authenticated: true
             })
             .when('/notes/create', {
                 templateUrl:'app/views/pages/template/add.html',
-                controller:'boardCtrl'
+                controller:'boardCtrl',
+                authenticated: true
             })
             .when('/notes/:id/edit', {
                 templateUrl:'app/views/pages/template/edit.html',
-                controller:'boardCtrl'
+                controller:'boardCtrl',
+                authenticated: true
             })
             .when('/notes/:id/show', {
                 templateUrl:'app/views/pages/template/show.html',
-                controller:'boardCtrl'
+                controller:'boardCtrl',
+                authenticated: true
             })
 
             // "catch all" to redirect to home page
@@ -56,3 +60,29 @@ angular.module('appRoutes', ['ngRoute'])
         // Required for no base (remove '#' from address bar)
         $locationProvider.html5Mode({enabled: true, requireBase: false});
     });
+
+app.run(['$rootScope', 'Auth', '$location',  function($rootScope, Auth, $location) {
+
+    // Check each time route changes
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+
+        // Only perform if user visited a route listed above
+        console.log(next.$$route);
+        if (next.$$route !== undefined) {
+            // Check if authentication is required on route
+            if (next.$$route.authenticated === true) {
+                // Check if authentication is required, then if permission is required
+                if (!Auth.isLoggedIn()) {
+                    event.preventDefault(); // If not logged in, prevent accessing route
+                    $location.path('/'); // Redirect to home instead
+                }
+            } else if (next.$$route.authenticated === false) {
+                // If authentication is not required, make sure is not logged in
+                if (Auth.isLoggedIn()) {
+                    event.preventDefault(); // If user is logged in, prevent accessing route
+                    $location.path('/profile'); // Redirect to profile instead
+                }
+            }
+        }
+    });
+}]);
