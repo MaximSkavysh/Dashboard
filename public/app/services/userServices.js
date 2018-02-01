0620770e1a0a056af6fe8d207b268aa393ad282a
MaximSkavysh <maximgen666@gmail.com>
2018-02-02 01:08:46 +0300

    fix problem with token timeout

--- public/app/services/userServices.js
+++ public/app/services/userServices.js
@@ -7,6 +7,9 @@ angular.module('userServices', [])
 		userFactory.create = function(regData) {
 			return $http.post('/api/users', regData); // Return data from end point to controller
 		};
+		userFactory.renewSession =function (username) {
+			return $http.get('/api//renewToken/' + username);
+        };
 
 		return userFactory;
 	});
