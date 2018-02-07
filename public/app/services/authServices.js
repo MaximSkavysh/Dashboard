3c57ae7cca1f1dd185bfb4951b015aa615fa6af3
MaximSkavysh <maximgen666@gmail.com>
2018-02-08 00:19:34 +0300

    not finished editing of user

--- public/app/services/authServices.js
+++ public/app/services/authServices.js
@@ -1,10 +1,10 @@
 angular.module('authServices', [])
 
-    .factory('Auth', function($http, AuthToken){
-         var authFactory = {};	//user objects
+    .factory('Auth', function ($http, AuthToken) {
+        var authFactory = {};	//user objects
         //User.create(regData):
-        authFactory.login = function(loginData){
-            return $http.post('/api/authenticate', loginData).then(function(data){
+        authFactory.login = function (loginData) {
+            return $http.post('/api/authenticate', loginData).then(function (data) {
                 //console.log(data.data.token);
                 AuthToken.setToken(data.data.token);
                 return data;
@@ -12,26 +12,26 @@ angular.module('authServices', [])
         };
 
         //Auth.isLoggedIn();
-        authFactory.isLoggedIn = function(){
-            if(AuthToken.getToken()){
+        authFactory.isLoggedIn = function () {
+            if (AuthToken.getToken()) {
                 return true;
             }
-            else{
+            else {
                 return false;
             }
         };
 
         //Auth.getUser();
-        authFactory.getUser = function(){
-            if(AuthToken.getToken()){
+        authFactory.getUser = function () {
+            if (AuthToken.getToken()) {
                 return $http.post('/api/current_user');
             }
-            else{
+            else {
                 $q.reject({message: 'faculty has no token'});
             }
         };
         //Auth.logout()
-        authFactory.logout = function(){
+        authFactory.logout = function () {
             //destroy token
             //no longer sets the token
             AuthToken.setToken();
@@ -40,19 +40,19 @@ angular.module('authServices', [])
         return authFactory;
     })
 
-    .factory('AuthToken', function($window){
-        var authTokenFactory ={};	//create factory object
-        authTokenFactory.setToken = function(token){
-            if(token){
+    .factory('AuthToken', function ($window) {
+        var authTokenFactory = {};	//create factory object
+        authTokenFactory.setToken = function (token) {
+            if (token) {
                 window.localStorage.setItem('token', token);
             }
-            else{
+            else {
                 window.localStorage.removeItem('token');
             }
         };
 
         //AuthToken.getToken();
-        authTokenFactory.getToken = function(){
+        authTokenFactory.getToken = function () {
             return $window.localStorage.getItem('token');
         };
         return authTokenFactory;
--- public/app/services/userServices.js
+++ public/app/services/userServices.js
@@ -1,24 +1,28 @@
 angular.module('userServices', [])
 
-	.factory('User', function($http) {
-		var userFactory = {}; // Create the User object
+    .factory('User', function ($http) {
+        var userFactory = {}; // Create the User object
 
-		// User.create(regData)
-		userFactory.create = function(regData) {
-			return $http.post('/api/users', regData); // Return data from end point to controller
-		};
-		userFactory.renewSession =function (username) {
-			return $http.get('/api/renewToken/' + username);
+        // User.create(regData)
+        userFactory.create = function (regData) {
+            return $http.post('/api/users', regData); // Return data from end point to controller
         };
-		userFactory.getPermission = function () {
-			return $http.get('/api/permission');
+        userFactory.renewSession = function (username) {
+            return $http.get('/api/renewToken/' + username);
+        };
+        userFactory.getPermission = function () {
+            return $http.get('/api/permission');
         }
         userFactory.getUsers = function () {
-          return $http.get('/api/management');
+            return $http.get('/api/management');
         };
-		userFactory.deleteUser = function (username) {
+        userFactory.getUser = function (id) {
+            return $http.get('/api/edit/' + id);
+        };
+
+        userFactory.deleteUser = function (username) {
             return $http.delete('/api/management/' + username);
         };
 
-		return userFactory;
-	});
+        return userFactory;
+    });
